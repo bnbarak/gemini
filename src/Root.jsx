88f18aceafbf444582a2d07533f9f69452250671
Barak Ben Noon <bn.barak@gmail.com>
2019-07-01 21:58:53 -0400

    css loader for antd

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import AppRouter from 'Components/AppRouter';
+import 'antd/dist/antd.css';
 
 const Root = ({ store }) => (
   <Provider store={store}>
